b75c57faeb721fa02ee2cb58fe7a6837dbbd92e9
sajers <62805020+sajers@users.noreply.github.com>
2020-03-29 14:51:23 +0000

    Update BaseUpgradeabilityProxy.sol

--- contracts/libraries/openzeppelin-upgradeability/BaseUpgradeabilityProxy.sol
+++ contracts/libraries/openzeppelin-upgradeability/BaseUpgradeabilityProxy.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "./Proxy.sol";
-import "openzeppelin-solidity/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-ethereum-package/blob/master/contracts/utils/Address.sol";
 
 /**
  * @title BaseUpgradeabilityProxy
